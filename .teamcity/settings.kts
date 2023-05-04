deaeeb7ad3e33ab4a40840b480f9c3d3165e19ec
saichandana <saichandana@TeamCity>
2023-05-04 00:24:24 +0000

    TeamCity change in 'Mono Repo (1)' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.dotnetBuild
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -36,6 +37,15 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        dotnetBuild {
+            name = "Build api"
+            projects = "ASPNETCoreWebAPI/SampleWebApiAspNetCore.sln"
+            outputDir = "./build"
+            param("dotNetCoverage.dotCover.home.path", "%teamcity.tool.JetBrains.dotCover.CommandLineTools.DEFAULT%")
+        }
     }
 
     triggers {
